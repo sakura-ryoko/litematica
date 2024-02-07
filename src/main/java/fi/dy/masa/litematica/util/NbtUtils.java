34b0abd96dbb19d240dd0f48ed74e29e78ca6afd
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-02-06 19:10:34 -0500

    24w05b -- Add 0.17.1 code

--- src/main/java/fi/dy/masa/litematica/util/NbtUtils.java
+++ src/main/java/fi/dy/masa/litematica/util/NbtUtils.java
@@ -13,6 +13,7 @@ import net.minecraft.util.math.Vec3d;
 
 import fi.dy.masa.litematica.Litematica;
 import fi.dy.masa.malilib.util.Constants;
+import net.minecraft.util.math.Vec3i;
 
 public class NbtUtils
 {
@@ -48,6 +49,22 @@ public class NbtUtils
             if (tagList.getHeldType() == Constants.NBT.TAG_DOUBLE && tagList.size() == 3)
             {
                 return new Vec3d(tagList.getDouble(0), tagList.getDouble(1), tagList.getDouble(2));
+            }
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public static Vec3i readVec3iFromIntArray(@Nullable NbtCompound tag, String tagName)
+    {
+        if (tag != null && tag.contains(tagName, Constants.NBT.TAG_INT_ARRAY))
+        {
+            int[] arr =  tag.getIntArray(tagName);
+
+            if (arr != null && arr.length == 3)
+            {
+                return new Vec3i(arr[0], arr[1], arr[2]);
             }
         }
 
