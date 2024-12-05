0f4502a90dedfb0b68e46f530ce4dbbf19f5fa3a
Sakura Ryoko <sakura@ryoko.soy>
2024-12-05 01:52:39 -0500

    fix: InfoOverlay getting confused with the ClientWorld feat/fix: add switchable AO Processor config,`renderAOModernEnabled` in order to disable / enable the 'new' AO Processor code; or continue using the older code from 2018 without the "Shadows" that it creates in between block layers.

--- src/main/java/fi/dy/masa/litematica/world/WorldSchematic.java
+++ src/main/java/fi/dy/masa/litematica/world/WorldSchematic.java
@@ -14,6 +14,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.DimensionEffects;
 import net.minecraft.component.type.MapIdComponent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.map.MapState;
