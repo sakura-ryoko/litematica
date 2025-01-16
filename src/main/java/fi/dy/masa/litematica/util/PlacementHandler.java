c283b6de7d0610f1df78627b37b715b793552a90
Sakura Ryoko <sakura@ryoko.soy>
2025-01-15 22:44:03 -0500

    sync up with MaLiLib & tidy up Easy Place

--- src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
+++ src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
@@ -39,9 +39,8 @@ public class PlacementHandler
             Properties.INVERTED,
             Properties.OPEN,
             //Properties.PERSISTENT,
-            // TODO --> TEST (Boolean)
-            Properties.POWERED,
-            Properties.LOCKED,
+            //Properties.POWERED,
+            //Properties.LOCKED,
             // EnumProperty:
             // ATTACHMENT - Bells
             // AXIS - Pillar
@@ -79,6 +78,14 @@ public class PlacementHandler
             Properties.DELAY,
             Properties.NOTE,
             Properties.ROTATION
+    );
+
+    /**
+     * BlackList for Block States.  Entries here will be reset to their default value.
+     */
+    public static final ImmutableSet<Property<?>> BLACKLISTED_PROPERTIES = ImmutableSet.of(
+            Properties.WATERLOGGED,
+            Properties.POWERED
     );
 
     public static EasyPlaceProtocol getEffectiveProtocolVersion()
@@ -238,8 +245,20 @@ public class PlacementHandler
             for (Property<?> p : propList)
             {
                 if ((property.isPresent() && !property.get().equals(p)) ||
-                    (property.isEmpty()) &&
-                    WHITELISTED_PROPERTIES.contains(p))
+                        (property.isEmpty()) &&
+                                WHITELISTED_PROPERTIES.contains(p))
+                    //WHITELISTED_PROPERTIES.contains(p) &&
+                    //!BLACKLISTED_PROPERTIES.contains(p))
+
+                /*
+                if (property.isPresent() && property.get().equals(p))
+                {
+                    //System.out.printf("[PHv3] skipping prot val: 0x%08X [Property %s]\n", protocolValue, p.getName());
+                    continue;
+                }
+                else if (WHITELISTED_PROPERTIES.contains(p) &&
+                        !BLACKLISTED_PROPERTIES.contains(p))
+                 */
                 {
                     @SuppressWarnings("unchecked")
                     Property<T> prop = (Property<T>) p;
@@ -281,6 +300,20 @@ public class PlacementHandler
         catch (Exception e)
         {
             Litematica.logger.warn("Exception trying to apply placement protocol value", e);
+        }
+
+        // Strip Blacklisted properties, and use the Block's default state.
+        // This needs to be done after the initial loop, or it breaks compatibility
+        for (Property<?> p : BLACKLISTED_PROPERTIES)
+        {
+            if (state.contains(p))
+            {
+                @SuppressWarnings("unchecked")
+                Property<T> prop = (Property<T>) p;
+                BlockState def = state.getBlock().getDefaultState();
+                state = state.with(prop, def.get(prop));
+                //System.out.printf("[PHv3] blacklisted state [%s] found, setting default value\n", prop.getName());
+            }
         }
 
         if (state.canPlaceAt(context.getWorld(), context.getPos()))
--- src/main/java/fi/dy/masa/litematica/util/WorldUtils.java
+++ src/main/java/fi/dy/masa/litematica/util/WorldUtils.java
@@ -880,9 +880,23 @@ public class WorldUtils
         {
             for (Property<?> p : propList)
             {
+                //System.out.printf("(WorldUtils):v3: check property [%s], whitelisted [%s], blacklisted [%s]\n", p.getName(), PlacementHandler.WHITELISTED_PROPERTIES.contains(p), PlacementHandler.BLACKLISTED_PROPERTIES.contains(p));
+
+                /*
                 if ((property.isPresent() && !property.get().equals(p)) ||
                     (property.isEmpty()) &&
                     PlacementHandler.WHITELISTED_PROPERTIES.contains(p))
+                    //PlacementHandler.WHITELISTED_PROPERTIES.contains(p) &&
+                    //!PlacementHandler.BLACKLISTED_PROPERTIES.contains(p))
+                 */
+
+                if (property.isPresent() && property.get().equals(p))
+                {
+                    //System.out.printf("(WorldUtils):v3: skipping prot val: 0x%08X [Property %s]\n", protocolValue, p.getName());
+                    continue;
+                }
+                if (PlacementHandler.WHITELISTED_PROPERTIES.contains(p) &&
+                    !PlacementHandler.BLACKLISTED_PROPERTIES.contains(p))
                 {
                     @SuppressWarnings("unchecked")
                     Property<T> prop = (Property<T>) p;
@@ -902,6 +916,12 @@ public class WorldUtils
                         ++propCount;
                     }
                 }
+                /*
+                else
+                {
+                    System.out.printf("(WorldUtils):v3: skipping prot val: 0x%08X [Property %s]\n", protocolValue, p.getName());
+                }
+                 */
             }
         }
         catch (Exception e)
