a68efd570baf9b27e0fc8c639a9d11d42e7a634e
Sakura Ryoko <sakura@ryoko.soy>
2025-12-31 14:21:51 -0500

    add: SchematicPlacementEventListener
    add the Placement SubRegion events
    
    add ALL_EVENTS flag
    
    more Placement event / invoker adjustments
    
    more Placement event / invoker adjustments
    
    cleanup Listener interface a bit
    
    add: hashCode to each Placement

--- src/main/java/fi/dy/masa/litematica/schematic/placement/SchematicPlacementManager.java
+++ src/main/java/fi/dy/masa/litematica/schematic/placement/SchematicPlacementManager.java
@@ -305,7 +305,13 @@ public class SchematicPlacementManager
         {
             this.schematicPlacements.add(placement);
             this.addTouchedChunksFor(placement);
+            ((SchematicPlacementEventHandler) SchematicPlacementEventHandler.getInstance()).onPlacementAdded(placement);
             this.onPlacementAdded();
+
+            if (this.selectedPlacement == null)
+            {
+                this.setSelectedSchematicPlacement(placement);
+            }
 
             if (printMessages)
             {
@@ -435,6 +441,7 @@ public class SchematicPlacementManager
     {
         if (placement == null || this.schematicPlacements.contains(placement))
         {
+            ((SchematicPlacementEventHandler) SchematicPlacementEventHandler.getInstance()).onPlacementSelected(this.selectedPlacement, placement);
             this.selectedPlacement = placement;
             OverlayRenderer.getInstance().updatePlacementCache();
             // Forget the last viewed material list when changing the placement selection
@@ -599,6 +606,8 @@ public class SchematicPlacementManager
 
     protected void onPlacementModified(SchematicPlacement placement)
     {
+        ((SchematicPlacementEventHandler) SchematicPlacementEventHandler.getInstance()).onPlacementUpdated(placement);
+
         if (placement.isEnabled())
         {
             OverlayRenderer.getInstance().updatePlacementCache();
