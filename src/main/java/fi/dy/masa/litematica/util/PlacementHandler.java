hi5ilay/gitmatica

fix: black listed states

src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 import net.minecraft.client.Minecraft;
@@ -85,9 +86,9 @@ public class PlacementHandler
     /**
      * BlackList for Block States.  Entries here will be reset to their default value.
      */
-    public static final ImmutableSet<Property<?>> BLACKLISTED_PROPERTIES = ImmutableSet.of(
-            BlockStateProperties.WATERLOGGED,
-            BlockStateProperties.POWERED
+    public static final ImmutableMap<Property<?>, ? extends Comparable<?>> BLACKLISTED_PROPERTIES = ImmutableMap.of(
+            BlockStateProperties.WATERLOGGED,       Boolean.FALSE,
+            BlockStateProperties.POWERED,           Boolean.FALSE
     );
 
     public static EasyPlaceProtocol getEffectiveProtocolVersion()
@@ -263,7 +264,7 @@ public class PlacementHandler
                     continue;
                 }
                 else if (WHITELISTED_PROPERTIES.contains(p) &&
-                        !BLACKLISTED_PROPERTIES.contains(p))
+                        !BLACKLISTED_PROPERTIES.containsKey(p))
                 {
                     @SuppressWarnings("unchecked")
                     Property<T> prop = (Property<T>) p;
@@ -321,15 +322,15 @@ public class PlacementHandler
 
         // Strip Blacklisted properties, and use the Block's default state.
         // This needs to be done after the initial loop, or it breaks compatibility
-        for (Property<?> p : BLACKLISTED_PROPERTIES)
+        for (Property<?> p : BLACKLISTED_PROPERTIES.keySet())
         {
             if (state.hasProperty(p))
             {
                 @SuppressWarnings("unchecked")
                 Property<T> prop = (Property<T>) p;
-                BlockState def = state.getBlock().defaultBlockState();
-                state = state.setValue(prop, def.getValue(prop));
-                //System.out.printf("[PHv3] blacklisted state [%s] found, setting default value\n", prop.getName());
+//                BlockState def = state.getBlock().defaultBlockState();
+                state = state.setValue(prop, (T) BLACKLISTED_PROPERTIES.get(p));
+                //System.out.printf("[PHv3] blacklisted state [%s] found, setting value\n", prop.getName());
             }
         }
 

sakura-ryoko/litematica

src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.*;
 import net.minecraft.block.enums.BlockHalf;
@@ -85,9 +86,9 @@ public class PlacementHandler
     /**
      * BlackList for Block States.  Entries here will be reset to their default value.
      */
-    public static final ImmutableSet<Property<?>> BLACKLISTED_PROPERTIES = ImmutableSet.of(
-            Properties.WATERLOGGED,
-            Properties.POWERED
+    public static final ImmutableMap<Property<?>, ? extends Comparable<?>> BLACKLISTED_PROPERTIES = ImmutableMap.of(
+            Properties.WATERLOGGED,       Boolean.FALSE,
+            Properties.POWERED,           Boolean.FALSE
     );
 
     public static EasyPlaceProtocol getEffectiveProtocolVersion()
@@ -271,7 +272,7 @@ public class PlacementHandler
                     continue;
                 }
                 else if (WHITELISTED_PROPERTIES.contains(p) &&
-                        !BLACKLISTED_PROPERTIES.contains(p))
+                        !BLACKLISTED_PROPERTIES.containsKey(p))
                 {
                     @SuppressWarnings("unchecked")
                     Property<T> prop = (Property<T>) p;
@@ -330,14 +331,14 @@ public class PlacementHandler
 
         // Strip Blacklisted properties, and use the Block's default state.
         // This needs to be done after the initial loop, or it breaks compatibility
-        for (Property<?> p : BLACKLISTED_PROPERTIES)
+        for (Property<?> p : BLACKLISTED_PROPERTIES.keySet())
         {
             if (state.contains(p))
             {
                 @SuppressWarnings("unchecked")
                 Property<T> prop = (Property<T>) p;
-                BlockState def = state.getBlock().getDefaultState();
-                state = state.with(prop, def.get(prop));
+//                BlockState def = state.getBlock().getDefaultState();
+                state = state.with(prop, (T) BLACKLISTED_PROPERTIES.get(p));
                 //System.out.printf("[PHv3] blacklisted state [%s] found, setting default value\n", prop.getName());
             }
         }