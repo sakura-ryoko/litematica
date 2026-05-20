fe3f3910935a5f7dfbe850c097140309dd89ba43
Sakura Ryoko <sakura@ryoko.soy>
2026-05-19 21:39:51 -0400

    fix: command-based pasting of signs by splitting the front/back text into sub 'data merge' commands. fix: Item frame command-based pasting by removing the 1.20.x logic, and injecting the corrected NBT.  If the NBT is too long; chop it all off except for any Custom Names / Dye Colors if possible.  If not possible, only a basic item with Zero NBT.
    fix: setToolItem() ??
    
    fix: warn when pasting with Oversized files
    
    Sync Lang files
    
    Sync various fixes from 1.21.11+
    
    Sync various fixes from 1.21.11+

--- src/main/java/fi/dy/masa/litematica/schematic/transmit/SchematicBufferManager.java
+++ src/main/java/fi/dy/masa/litematica/schematic/transmit/SchematicBufferManager.java
@@ -1,7 +1,8 @@
 package fi.dy.masa.litematica.schematic.transmit;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
 
 import net.minecraft.nbt.NbtCompound;
@@ -13,26 +14,26 @@ import fi.dy.masa.litematica.util.FileType;
 
 public class SchematicBufferManager
 {
-    private final HashMap<Long, SchematicBuffer> fileBuffers;
-    private final HashMap<Long, NbtCompound> optionalNbt;
+    private final ConcurrentHashMap<Long, SchematicBuffer> fileBuffers;
+    private final ConcurrentHashMap<Long, NbtCompound> optionalNbt;
 
     public SchematicBufferManager()
     {
-        this.fileBuffers = new HashMap<>();
-        this.optionalNbt = new HashMap<>();
+        this.fileBuffers = new ConcurrentHashMap<>(16, 0.9f, 1);
+        this.optionalNbt = new ConcurrentHashMap<>(16, 0.9f, 1);
     }
 
-    public void createBuffer(String name, final long sessionKey)
+    public void createBuffer(int totalExpectedSlices, long totalExpectedSize, final long sessionKey)
     {
-        this.createBuffer(name, FileType.LITEMATICA_SCHEMATIC, sessionKey, null);
+        this.createBuffer(totalExpectedSlices, totalExpectedSize, FileType.LITEMATICA_SCHEMATIC, sessionKey, null);
     }
 
-    public void createBuffer(String name, final long sessionKey, @Nullable NbtCompound optional)
+    public void createBuffer(int totalExpectedSlices, long totalExpectedSize, final long sessionKey, @Nullable NbtCompound optional)
     {
-        this.createBuffer(name, FileType.LITEMATICA_SCHEMATIC, sessionKey, optional);
+        this.createBuffer(totalExpectedSlices, totalExpectedSize, FileType.LITEMATICA_SCHEMATIC, sessionKey, optional);
     }
 
-    public void createBuffer(String name, FileType type, final long sessionKey, @Nullable NbtCompound optional)
+    public void createBuffer(int totalExpectedSlices, long totalExpectedSize, FileType type, final long sessionKey, @Nullable NbtCompound optional)
     {
         if (this.fileBuffers.containsKey(sessionKey) || this.optionalNbt.containsKey(sessionKey))
         {
@@ -40,7 +41,7 @@ public class SchematicBufferManager
             return;
         }
 
-        SchematicBuffer newBuf = new SchematicBuffer(name, type);
+        SchematicBuffer newBuf = new SchematicBuffer(totalExpectedSlices, totalExpectedSize, type);
         this.fileBuffers.put(sessionKey, newBuf);
 
         if (optional != null && !optional.isEmpty())
@@ -85,9 +86,9 @@ public class SchematicBufferManager
     {
         if (this.fileBuffers.containsKey(sessionKey))
         {
-            try (SchematicBuffer buffer = this.fileBuffers.remove(sessionKey))
+            try
             {
-                buffer.close();
+                this.fileBuffers.remove(sessionKey);
             }
             catch (Exception ignored) {}
         }
@@ -110,12 +111,22 @@ public class SchematicBufferManager
 
             if (file == null)
             {
-                Litematica.LOGGER.error("finishBuffer: Failed writing Schematic Buffer to file: '{}'", buffer.getFileName());
+                Litematica.LOGGER.error("finishBuffer: Failed writing Schematic Buffer to file: '{}'", buffer.getFileNameWithExt());
                 return null;
             }
 
-            LitematicaSchematic schematic = LitematicaSchematic.createFromFile(dir, buffer.getName(), buffer.getType());
+            LitematicaSchematic schematic = LitematicaSchematic.createFromFile(dir, buffer.getFileName(), buffer.getType());
             this.cancelBuffer(sessionKey);
+
+            if (schematic == null)
+            {
+                try
+                {
+                    Files.delete(file);
+                }
+                catch (Exception ignored) {}
+            }
+
             return schematic;
         }
 
