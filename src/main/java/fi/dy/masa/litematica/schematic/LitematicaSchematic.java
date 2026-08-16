6df36f4acff79a7622448c307e5107d5b430dc5d
Sakura Ryoko <sakura@ryoko.soy>
2026-08-16 18:00:44 -0400

    oops

--- src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
+++ src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
@@ -2525,8 +2525,6 @@ public class LitematicaSchematic
 //                BlockState state = net.minecraft.nbt.NbtUtils.readBlockState(lookup, t);
 				BlockState state = DataTypeUtils.readBlockStateFromTag(t, SchematicWorldHandler.INSTANCE.getRegistryManager());
 
-				System.out.printf("readFromVanillaStructure: stateIn: %s\n", state.toString());
-
 				list.add(state);
 			}
 
