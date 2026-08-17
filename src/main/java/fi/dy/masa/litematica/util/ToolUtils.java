a701d41f68644b31e7e8a33ed5232232139afdb8
Sakura Ryoko <sakura@ryoko.soy>
2026-08-16 22:01:40 -0400

    Shorten "Submitted to Servux" message

--- src/main/java/fi/dy/masa/litematica/util/ToolUtils.java
+++ src/main/java/fi/dy/masa/litematica/util/ToolUtils.java
@@ -72,7 +72,7 @@ public class ToolUtils
                     {
                         data.put("Boxes", list);
                         EntityDataManager.getInstance().sendServuxTaskRequest(data, new InfoHudSync(null));
-                        InfoUtils.showGuiOrInGameMessage(MessageType.INFO, "litematica.message.scheduled_task_to_servux");
+                        InfoUtils.showGuiOrInGameMessage(MessageType.INFO, 2500, "litematica.message.scheduled_task_to_servux");
                     }
                     else
                     {
@@ -158,7 +158,7 @@ public class ToolUtils
                     {
                         data.put("Boxes", list);
                         EntityDataManager.getInstance().sendServuxTaskRequest(data, new InfoHudSync(listener));
-                        InfoUtils.showGuiOrInGameMessage(MessageType.INFO, "litematica.message.scheduled_task_to_servux");
+                        InfoUtils.showGuiOrInGameMessage(MessageType.INFO, 2500, "litematica.message.scheduled_task_to_servux");
                     }
                     else
                     {
